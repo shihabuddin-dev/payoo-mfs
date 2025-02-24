4c9aee2aaea464a5917f25c30ba8ff58b3412790
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-24 21:30:08 +0600

    added new file and make with pagination and made few things

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById('login-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const accountNumber = document.getElementById('account-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        const convertedPin = parseInt(pinNumber);
+        if (accountNumber.length === 11) {
+            if (convertedPin === 1234) {
+                window.location.href='./main.html';
+            }
+            else {
+               alert('Wrong Pin');
+            }
+        }
+        else {
+            alert('Invalid Account Number');
+        }
+    });
