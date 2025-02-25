4dbf68015ad677c8df5b03db31843767c1ef6656
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-25 14:45:10 +0600

    added transaction and transfer money

--- script/toggle.js
+++ script/toggle.js
@@ -1,12 +1,26 @@
-document.getElementById('addMoney').style.display= 'none';
+document.getElementById('transferMoney').style.display = 'block';
+document.getElementById('addMoney').style.display = 'none';
+document.getElementById('cashOut').style.display = 'none';
+// add money section 
 document.getElementById('add-money-option')
-.addEventListener('click', function(){
-    document.getElementById('addMoney').style.display= 'block';
-    document.getElementById('cashOut').style.display= 'none';
-});
+    .addEventListener('click', function () {
+        document.getElementById('addMoney').style.display = 'block';
+        document.getElementById('cashOut').style.display = 'none';
+        document.getElementById('transferMoney').style.display = 'none';
+    });
+// CashOut section 
 document.getElementById('cash-out-option')
-.addEventListener('click', function(){
-    document.getElementById('addMoney').style.display= 'none';
-    document.getElementById('cashOut').style.display= 'block';
+    .addEventListener('click', function () {
+        document.getElementById('cashOut').style.display = 'block';
+        document.getElementById('addMoney').style.display = 'none';
+        document.getElementById('transferMoney').style.display = 'none';
+    });
+// transfer Money section
+document.getElementById('transfer-money-option').addEventListener('click', function (event) {
+    document.getElementById('transferMoney').style.display = 'block';
+    document.getElementById('addMoney').style.display = 'none';
+    document.getElementById('cashOut').style.display = 'none';
 });
-// cash-out-option 
+// get-bonus-option
+// pay-bill-option
+// tr-history-option 
---
+++ script/transferMoney.js
@@ -0,0 +1,31 @@
+// user-account-number transfer-amount transfer-pin-number 
+document.getElementById('btn-transfer')
+    .addEventListener('click', function (event) {
+        const userAccount = document.getElementById('user-account-number').value;
+        // const convertedAccount = parseInt(userAccount);
+        const transferAmount = document.getElementById('transfer-amount').value;
+        const convertedTrAmount = parseFloat(transferAmount);
+        const transferPin = document.getElementById('transfer-pin-number').value;
+        const convertedTrPin = parseInt(transferPin);
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const convertedMainBalance = parseFloat(mainBalance);
+        if (userAccount.length === 11) {
+            if (convertedTrAmount <= convertedMainBalance && convertedTrAmount > 0) {
+                if (convertedTrPin === 1234) {
+                    const currentBalance = convertedMainBalance - convertedTrAmount;
+                    document.getElementById('main-balance').innerText = currentBalance;
+                    alert(`Transfer Successful Your Balance is ${currentBalance}`);
+                }
+                else (
+                    alert('Pin Number did not Matched')
+                )
+            }
+            else {
+                alert('Please Enter Valid Amount')
+            }
+        }
+        else {
+            alert('Invalid Account Number')
+        }
+
+    })
